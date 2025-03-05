6225c82eda7859711a0381cb1097077b72c94c4a
loupatfer <loupatfer@gmail.com>
2025-03-05 13:32:06 +0000

    mensagem

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -7,6 +7,7 @@ public class HelloWorld extends JFrame {
         setContentPane(panel1);
         pack();
         System.out.println("Estive aqui");
+        System.out.println("Estou aqui loupatfer");
     }
     public static void main(String[] args) {
         new HelloWorld().setVisible(true);
